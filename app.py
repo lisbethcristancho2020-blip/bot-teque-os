f2fd674a8b4cabc30cb9fda8cbe324977a170d9e
lisbethcristancho2020-blip <lisbethcristancho2020@gmail.com>
2026-08-16 12:07:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -107,4 +107,5 @@ def enviar_mensaje_whapi(chat_id, texto):
     requests.post(WHAPI_URL, json=payload, headers=headers)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000
+    app.run(host='0.0.0.0', port=port)
